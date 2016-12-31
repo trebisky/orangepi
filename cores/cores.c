c5f5936464059fe7eff491cb0d2c54a1cb1f4eaf
Tom Trebisky <tom@mmto.org>
2016-12-30 18:01:30 -0700

    Still can't start second core

--- cores/cores.c
+++ cores/cores.c
@@ -298,8 +298,10 @@ delay_ms ( int msecs )
 #define PRCM_BASE   	0x01f01400
 
 #define ROM_START	((unsigned long *) 0x01f01da4)
-#define GEN_CTRL	((unsigned long *) 0x01f01d84)
-#define POWER_OFF	((unsigned long *) (PRCM_BASE+0x100))
+#define GEN_CTRL	((unsigned long *) (CPUCFG_BASE + 0x184))
+#define DBG_CTRL1	((unsigned long *) (CPUCFG_BASE + 0x1e4))
+
+#define POWER_OFF	((unsigned long *) (PRCM_BASE + 0x100))
 
 typedef void (*vfptr) ( void );
 
@@ -323,17 +325,34 @@ void
 launch_core ( int cpu, vfptr who )
 {
 	unsigned long *reset; 
+	unsigned long *clamp; 
+	unsigned long *enab; 
+	unsigned long mask = 1 << cpu;
+	int i;
 
-	reset = (unsigned long *) ( CPUCFG_BASE + (cpu+1) * 0x40);
+	reset = (unsigned long *) ( CPUCFG_BASE + 0x40 + cpu * 0x40);	/* reset */
+	clamp = (unsigned long *) ( PRCM_BASE + 0x140 + cpu * 4);	/* power clamp */
+	enab = (unsigned long *) ( PRCM_BASE + 0x10 + cpu * 4);		/* power clamp */
+
 	bounce = who;
 
-	*ROM_START = (unsigned long) new_core;
+	*ROM_START = (unsigned long) new_core;	/* in start.S */
 
 	*reset = 0;			/* put core into reset */
-	*GEN_CTRL &= ~(1<<cpu);		/* reset L1 cache */
-	*POWER_OFF &= ~(1<<cpu);	/* power on */
+
+	*GEN_CTRL &= ~mask;		/* reset L1 cache */
+	*DBG_CTRL1 &= ~mask;		/* sun6i - disable external debug */
+	// *enab = 0xffffffff;
+	*enab = 0;
+
+        for (i = 0; i <= 8; i++)	/* sun6i - power clamp */
+	    *clamp = 0xff >> i;
+
+	*POWER_OFF &= ~mask;		/* power on */
 	delay_ms ( 2 );			/* delay at least 1 ms */
+
 	*reset = 3;
+	*DBG_CTRL1 |= mask;		/* sun6i - reenable external debug */
 }
 
 void
